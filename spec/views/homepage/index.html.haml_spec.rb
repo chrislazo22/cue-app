e0ee70144a6aeb8411ed11ac133cca0f69884be8
Christian Lazo <lazochristian.cl@gmail.com>
2018-05-20 13:48:26 -0700

    Add homepage view

---
+++ spec/views/homepage/index.html.haml_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'homepage', type: :view do
+  describe 'Page Header' do
+    it 'render the H1 page header' do
+    end
+  end
+end
