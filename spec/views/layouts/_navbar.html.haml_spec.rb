c163bb3b0fb5d4e228ef8fbefe0f5ee90965fa54
Christian Lazo <lazochristian.cl@gmail.com>
2018-05-20 15:39:27 -0700

    Add navbar

---
+++ spec/views/layouts/_navbar.html.haml_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'layouts/_navbar', type: :view do
+  it 'has a title' do
+    render
+    assert_select 'nav h1', text: 'cue', count: 1
+  end
+end
