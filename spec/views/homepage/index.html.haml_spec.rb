da614bc1d5c7bf936e993b3108015e36134ba824
Christian Lazo <lazochristian.cl@gmail.com>
2018-05-20 15:58:19 -0700

    Add link to homepage

--- spec/views/homepage/index.html.haml_spec.rb
+++ spec/views/homepage/index.html.haml_spec.rb
@@ -2,9 +2,9 @@
 
 require 'rails_helper'
 
-RSpec.describe 'homepage', type: :view do
-  describe 'Page Header' do
-    it 'render the H1 page header' do
-    end
+RSpec.describe 'homepage/index', type: :view do
+  it 'has host sign in link' do
+    render
+    assert_select '.row.center a', text: 'Host Sign In', count: 1
   end
 end
