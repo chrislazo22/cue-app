3aafb71bd058d392f5ac7da4a2b5b822eb864e51
Christian Lazo <lazochristian.cl@gmail.com>
2018-06-11 21:51:50 -0700

    Modify spec

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe HomepageController, type: :controller do
-  describe 'Get #index' do
+  describe 'get #index' do
     it 'has a 200 status code' do
       get :index
       expect(response.status).to eq(200)
--- spec/controllers/hosts_controller_spec.rb
+++ spec/controllers/hosts_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HostsController, type: :controller do
-
+  describe 'get #index' do
+    it 'has a 200 status code' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
 end
