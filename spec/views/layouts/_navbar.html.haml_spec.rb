bcc5185cda41908ec1280b830b8e5c10274500d1
Christian Lazo <lazochristian.cl@gmail.com>
2018-11-25 18:17:34 -0800

    Update navbar

--- spec/views/layouts/_navbar.html.haml_spec.rb
+++ spec/views/layouts/_navbar.html.haml_spec.rb
@@ -3,8 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe 'layouts/_navbar', type: :view do
- it 'has a title' do
+  it 'has a title' do
    render
    assert_select 'nav', count: 1
+  end
+
+  it 'renders the home link' do
+    render
+    assert_select '.navbar-nav li a', text: 'Home', count: 1
+  end
+
+  it 'renders the products link' do
+    render
+    assert_select '.navbar-nav li a', text: 'Products', count: 1
+  end
+
+
+  it 'renders the Learn More link' do
+    render
+    assert_select '.navbar-nav li a', text: 'Learn More', count: 1
   end
 end
